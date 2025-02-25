5dbe379d26ca416b2f6d834f417a0a3028afb42f
Peter Levinsky <pele@zealand.dk>
2025-02-25 13:36:04 +0100

    Add project files.

---
+++ ClassDemoMusicLib/model/Music.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoMusicLib.model
+{
+    public class Music
+    {
+        // instance fields
+        private int _id;
+        private string _title;
+        private int _year;
+
+        // properties
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value is null || value.Length < 2)
+                {
+                    throw new ArgumentException("title must be at least 2 character");
+                }
+                _title = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                if (value < 1900)
+                {
+                    throw new ArgumentException("only for music after 1900");
+                }
+                _year = value;
+            }
+        }
+
+        // constructors
+        public Music(int id, string title, int year)
+        {
+            Id = id;
+            Title = title;
+            Year = year;
+            
+        }
+
+        public Music() : this(-1, "dummy", 1900)
+        {
+        }
+
+        // To String
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Title)}={Title}, {nameof(Year)}={Year.ToString()}}}";
+        }
+
+        
+
+    }
+}
---
+++ ClassDemoMusicLib/repositories/MusicRepository.cs
@@ -0,0 +1,66 @@
+﻿using ClassDemoMusicLib.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoMusicLib.repositories
+{
+    public class MusicRepository
+    {
+        // instance fields
+        private readonly List<Music> _musics;
+
+        // constructor
+        public MusicRepository()
+        {
+            _musics = new List<Music>();
+        }
+
+
+        /*
+         * CRUD operations
+         */
+        public List<Music> GetAll()
+        {
+            return new List<Music>(_musics);
+        }
+
+        public Music GetById(int id)
+        {
+            Music? Music = _musics.Find(m => m.Id == id);
+            if (Music == null)
+            {
+                throw new KeyNotFoundException();
+            }
+            return Music;
+        }
+        public Music Add(Music music)
+        {
+            music.Id = GenerateId();
+            _musics.Add(music);
+            return music;
+        }
+        public Music Delete(int id)
+        {
+            Music music = GetById(id);
+            _musics.Remove(music);
+            return music;
+        }
+        public Music Update(int id, Music updatedMusic)
+        {
+            Music music = GetById(id);
+            int ix = _musics.IndexOf(music);
+            _musics[ix] = updatedMusic;
+            return updatedMusic;
+        }
+
+        // help function
+        private int GenerateId()
+        {
+            return (_musics.Count == 0) ? 1 : _musics.Max(a => a.Id) + 1;
+        }
+    }
+}
